fills in matrix diagonal elements but sign is wrong

matrixRepCalc.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def matrix_rep(n):
     partitions = generate_partitions(n)
     tableaux_by_shape = [tableaux_shape(n, partition) for partition in partitions]
@@ -6,7 +9,11 @@ def matrix_rep(n):
         representation = []
         for shape in tableaux_by_shape:
             sort_tableaux(n, shape)
-            print(shape)    
+            rep = np.zeros((len(shape), len(shape)))
+            print(shape)
+            for index in range(len(shape)):
+               rep[index, index] = 1/(shape[index].signed_distance(i))
+            representation.append(rep)
         rho["(" + str(i) + "," + str(i+1) + ")"] = representation
     return rho
 
@@ -40,12 +47,12 @@ def rem_dubs_helper(partition):
 
 def generate_tableaux(n):
     if n == 1:
-        return Tableau([[1]])
+        return [Tableau([[1]])]
     else:
         prev_tab = generate_tableaux(n-1)
-        ans = [tab.data + [[n]] for tab in prev_tab]
+        ans = [Tableau(tab.data + [[n]]) for tab in prev_tab]
         for tab in prev_tab:
-            for i in range(0, tab.size):
+            for i in range(len(tab.data)):
                 if tab.size == 1:
                     ans += [Tableau([tab.data[i] + [n]])]
                 elif i == 0 or len(tab.data[i-1]) > len(tab.data[i]):
@@ -56,17 +63,15 @@ def generate_tableaux(n):
 
 def tableaux_shape(n, partition):
     ans = []
-    matching = True
     tableaux = generate_tableaux(n)
     for tab in tableaux:
-        if len(tab) == len(partition):
-            for row_index in range(len(tab)):
-                if len(tab[row_index]) != partition[row_index]:
+        if len(tab.data) == len(partition):
+            for row_index in range(len(tab.data)):
+                matching = True
+                if len(tab.data[row_index]) != partition[row_index]:
                     matching = False
             if matching:
                 ans += [tab]
-            else:
-                matching = True
     return ans
                 
 def sort_tableaux(n, tableaux):
@@ -74,7 +79,7 @@ def sort_tableaux(n, tableaux):
     while switched:
         switched = False
         for i in range(len(tableaux) - 1):
-            if tableaux[i] > tableaux[i+1]:
+            if tableaux[i] < tableaux[i+1]:
                 tableaux[i], tableaux[i+1] = tableaux[i+1], tableaux[i]
                 switched = True
     return
@@ -123,10 +128,30 @@ class Tableau(object):
                     return row, col
         return "Not in Tableau"
 
-    def distance(self, k):
-        kRow, kCol = self.find(k)
-        kPlusOneRow, kPlusOneCol = self.find(k+1) 
-        return abs(kRow - kPlusOneRow) + abs(kCol - kPlusOneCol)+1
+    def signed_distance(self, k):
+        k_row, k_col = self.find(k)
+        content_k = k_row - k_col
+        k_plus_one_row, k_plus_one_col = self.find(k+1) 
+        content_k_plus_one = k_plus_one_row - k_plus_one_col
+        return  content_k_plus_one - content_k
 
     def is_standard(self):
-        return
+        for row in range(len(self.data)-1):
+            for i in range(len(self.data[row+1])):
+                if self.data[row][i] > self.data[row+1][i]:
+                    return False
+            for i in range(len(self.data[row])-1):
+                if self.data[row][i] > self.data[row][i+1]:
+                    return False
+        for i in range(len(self.data[-1])-1):
+            if self.data[-1][i] > self.data[-1][i+1]:
+                    return False
+        return True
+
+    def switch(self, k):
+        switched_tableau = Tableau(self.data)
+        k_row, k_col = self.find(k)
+        k_plus_one_row, k_plus_one_col = self.find(k+1)
+        switched_tableau.data[k_row][k_col] = k+1
+        switched_tableau.data[k_plus_one_row][k_plus_one_col] = k
+        return switched_tableau