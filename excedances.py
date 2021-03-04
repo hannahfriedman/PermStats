77d255dac0f702e86e794a060dc4e5bdd2a952b6
Hannah Friedman <hannah@spottedsnake.net>
2021-03-04 08:32:07 -0800

    added matrix with span of vij to excedance file

--- excedances.py
+++ excedances.py
@@ -25,8 +25,54 @@ def generate_matrix(n):
                 if permList[col].__call__(i)==row + 2  and row + 2>i:
                     mat[row, col] = 1
     #for perm in permList:
-    #    print(perm.__str__())
+    #   print(perm.__str__())
     return mat
+
+def sn_act_on_matrix(n, i):
+    """
+    returns the matrix of excedance indicator functions for Sn after being acted on by (i,i+1)
+    """
+    mat = generate_matrix(n)
+    nfac = fac(n)
+    perms = Permutation.group(n)
+    switched = []
+    #These dictionaries allow us to access permutations by the index they represent and vice versa
+    perm_index_dict = {}
+    index_perm_dict = {}
+    perm_list = []
+    #Populate the dictionaries
+    for j in range(nfac):
+        #The following line is printing and i'm not sure how to make it stop
+        p = next(perms)
+        perm_index_dict[p] = j
+        index_perm_dict[j] = p
+        perm_list.append(p)
+    for col in range(len(mat[0])):
+        perm = index_perm_dict[col]
+        if perm not in switched:
+            perm_acted_on = perm*Permutation.cycle(i, i+1)
+            col2 = perm_index_dict[perm_acted_on]      
+            perm_list[col], perm_list[col2] = perm_list[col2], perm_list[col]
+            for row in range(n-1):
+                mat[(row, col)], mat[(row, col2)] = mat[(row, col2)], mat[(row, col)]
+            switched.append(perm_acted_on)
+    #for perm in perm_list:
+    #   print(perm.__str__())
+    return mat
+    
+def mega_matrix(n):
+    nfac = fac(n)
+    mat = np.zeros((n**2 - n, nfac))
+    for matrix_index in range(n):
+        if matrix_index == 0:
+            matrix = generate_matrix(n)
+        else:
+            matrix = sn_act_on_matrix(n, matrix_index)
+        for row in range(n-1):
+                mat[row + (n-1)*matrix_index] = matrix[row]
+    return mat
+
+
 
 def count_excedances(perm, n):
     count = 0
@@ -44,6 +90,6 @@ def count_excedances_wij(perm, n):
 
 
 def f(n):
-    mat = generate_matrix(n)
+    mat = mega_matrix(n)
     print(la.matrix_rank(mat))
 
