f2d557bd46c609e0ff20bea5ad83d7eab89714ca
Hannah Friedman <hannah@spottedsnake.net>
2021-03-24 11:39:17 -0700

    exponentiating matrices

--- var_dist.py
+++ var_dist.py
@@ -2,6 +2,7 @@ from matrixRepCalc import DFT_excedances
 from matrixRepCalc import DFT_major_index
 from permutation import Permutation
 from excedances import count_excedances
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -26,46 +27,47 @@ def var_dist(mat):
                 max_val = abs(mat[row,col])
     return max_val
 
-def __main__():
-    mat = DFT_excedances(5)[2]
-    mat = normalize(mat, total_exced(5))
-    print(var_dist(mat))
-    mat2 = np.linalg.matrix_power(mat, 2)
-    print(var_dist(mat2))
-    mat3 = np.linalg.matrix_power(mat, 3)
-    print(var_dist(mat3))
-    mat4 = np.linalg.matrix_power(mat, 4)
-    print(var_dist(mat4))
-    mat5 = np.linalg.matrix_power(mat, 5)
-    print(var_dist(mat5))
-    mat6 = np.linalg.matrix_power(mat, 6)
-    print(var_dist(mat6))
-    mat7 = np.linalg.matrix_power(mat, 7)
-    print(var_dist(mat7))
-    mat8 = np.linalg.matrix_power(mat, 8)
-    print(var_dist(mat8))
-    
+def plot_var_dist(mat, max_pow):
+    powers = []
+    for power in range(1, max_pow+1):
+        powers.append(var_dist(np.linalg.matrix_power(mat, power)))
+    plt.yscale('log')
+    plt.scatter(range(1, max_pow+1), powers)
+    plt.show()
+    return powers
 
-    '''
+def plot_var_dist(mat, mat1, max_pow):
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    plt.yscale('log')
+    powers = []
+    powers1 = []
+    for power in range(1, max_pow+1):
+        powers.append(var_dist(np.linalg.matrix_power(mat, power)))
+        powers1.append(var_dist(np.linalg.matrix_power(mat1, power)))
+    ax1.scatter(range(1, max_pow+1), powers)
+    ax1.scatter(range(1, max_pow+1),powers1)
+    plt.show()
+    return powers, powers1
+
+
+def __main__():
+    
+    #mat = DFT_excedances(5)[2]
+    #mat = normalize(mat, total_exced(5))
+    #plot_var_dist(mat, 20)
+    #print(DFT_excedances(6)) -- nonzero matrix index is 4
     mat = DFT_major_index(5)[2]
-    mat = normalize(mat, major_index(5))
-    print(mat)
-    print(var_dist(mat))
-    mat2 = np.linalg.matrix_power(mat, 2)
-    print(var_dist(mat2))
-    mat3 = np.linalg.matrix_power(mat, 3)
-    print(var_dist(mat3))
-    mat4 = np.linalg.matrix_power(mat, 4)
-    print(var_dist(mat4))
-    mat5 = np.linalg.matrix_power(mat, 5)
-    print(var_dist(mat5))
-    mat6 = np.linalg.matrix_power(mat, 6)
-    print(var_dist(mat6))
-    mat7 = np.linalg.matrix_power(mat, 7)
-    print(var_dist(mat7))
-    mat8 = np.linalg.matrix_power(mat, 8)
-    print(var_dist(mat8))
+    mat = normalize(mat, total_exced(5))
+    mat1 = DFT_major_index(5)[4]
+    mat1 = normalize(mat1, total_exced(5))
+    print(plot_var_dist(mat, mat1, 13))
     '''
+    plot_var_dist(mat, 20)
+    plot_var_dist(mat1, 20)
+    print(DFT_major_index(5)) 
+    '''
+
 
 
 __main__()
